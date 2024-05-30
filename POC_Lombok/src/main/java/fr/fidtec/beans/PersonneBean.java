d7aadaddb65cbc751d315d3562bc39bd6c0bd25a
Fidele COULON <fidele.coulon@hotmail.fr>
2024-05-30 18:59:50 +0200

    [feat] Add POC_Lombok

---
+++ POC_Lombok/src/main/java/fr/fidtec/beans/PersonneBean.java
@@ -0,0 +1,22 @@
+package fr.fidtec.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PersonneBean {
+
+	@Getter @Setter
+	private String nom;
+	
+	@Getter @Setter
+	private String prenom;
+			
+}
---
+++ POC_Lombok/src/test/java/fr/fidtec/beans/PersonneBean_Test.java
@@ -0,0 +1,21 @@
+package fr.fidtec.beans;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PersonneBean_Test {
+
+	@Test
+	public void CreatePersonne_Test() {
+		
+		PersonneBean p = new PersonneBean();
+		
+		p.setNom("COULON").setPrenom("Fidele");
+	
+		assertEquals("COULON", p.getNom());
+		assertEquals("Fidele", p.getPrenom());
+		
+		System.out.println(p.toString());
+	}
+}
