1ebdc4b54f3f8be837bb31ba5cd8b62f4f5b1244
Fidele COULON <fidele.coulon@hotmail.fr>
2024-11-16 21:12:31 +0100

    [feat] Test d'exclusion surefire dans JUnit5

--- POC_Junit5_Mockito/src/test/java/fr/fidtec/tests/KOTest.java
+++ POC_Junit5_Mockito/src/test/java/fr/fidtec/tests/KOTest.java
@@ -1,4 +1,13 @@
 package fr.fidtec.tests;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class KOTest {
+
+    @Test
+    void testOK() {
+        assertFalse(true);
+    }
 }
